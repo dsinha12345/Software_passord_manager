ddaec2f5ac863497e453a4801d875e8cb6b004e1
DBCoding87 <122477319+DBCoding87@users.noreply.github.com>
2024-10-21 15:40:39 -0400

    Reverted unnecessary changes

--- password/users/urls.py
+++ password/users/urls.py
@@ -1,9 +1,8 @@
 # users/urls.py
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
-    #path('', include('password_generator.urls')),  # Include the password_generator app URLs
 ]
